28d315d3c53031e49d5a738a8aecfe390432abbb
Felipe de Jesus Reyes Gonzalez <rgfelipe771@gmail.com>
2024-09-03 13:06:18 -0600

    primer comiteada

---
+++ js/app.js
@@ -0,0 +1,23 @@
+//aqaui comienza mi proyecto
+// calcular el promedio de personas registradas
+//al menos una condicional un ciclo t una funcion
+
+const title = document.getElementById("title");
+console.log(title);
+
+title.addEventListener("click", () => {
+  document.body.style.backgroundColor = "lightblue";
+});
+
+const nombre = prompt("escribeeeee aqi culero");
+
+console.log(nombre);
+
+/* const valor = nombre.addEventListener("enter", (e) => {
+  const content = e.target.value;
+  console.log("Event", e);
+  console.log("Event clkaaaa", e.target);
+  console.log("Event pforjkepo", e.target.value);
+  return content;
+});
+ */
